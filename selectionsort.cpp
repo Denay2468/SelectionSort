76f0013d0dd2725813863b8087e0d7934b853589
Denay2468 <denayaung13@gmail.com>
2026-04-09 20:09:40 +0630

    make input function with for loop and create selectionsort function

--- selectionsort.cpp
+++ selectionsort.cpp
@@ -13,4 +13,24 @@ void input(){
         else
             cout<<"Array can have maximum 20 elements. Please enter again."<<endl;
     }
+    cout<<"Enter Array elements:"<<endl;
+    for (int i=0; i<n; i++){
+        cout<<"Element "<<i+1<<": ";
+        cin>>arr[i];
+    }
+
 }
+
+void selectionSort(){
+    for (int i=0; i<n-1; i++){
+        int minIndex = i;
+        for (int j=i+1; j<n; j++){
+            if (arr[j] < arr[minIndex]){
+                minIndex = j;
+            }
+        }
+        swap(arr[i], arr[minIndex]);
+    }
+}
+
+
